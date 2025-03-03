1cf22969799fea013d9a390302ec871480020cb4
mvalsania <mvalsania25@cmc.edu>
2025-03-02 21:28:11 -0800

    second commit

--- sql/09.sql
+++ sql/09.sql
@@ -1,7 +1,9 @@
-SELECT staff.first_name, staff.last_name, SUM(payment.amount) AS sum
+SELECT staff.first_name,
+       staff.last_name,
+       SUM(payment.amount) AS sum
 FROM staff
 JOIN payment ON staff.staff_id = payment.staff_id
-WHERE payment.payment_date >= '2020-01-01'
-  AND payment.payment_date < '2020-02-01'
-GROUP BY staff.staff_id;
+WHERE DATE(payment.payment_date) >= '2020-01-01'
+  AND DATE(payment.payment_date) < '2020-02-01'
+GROUP BY staff.staff_id, staff.first_name, staff.last_name;
 
