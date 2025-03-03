bc79a19a84f7d99dc94fee0763a5b9d9cea73804
mvalsania <mvalsania25@cmc.edu>
2025-03-02 21:35:52 -0800

    third time is the charm

--- sql/09.sql
+++ sql/09.sql
@@ -3,7 +3,8 @@ SELECT staff.first_name,
        SUM(payment.amount) AS sum
 FROM staff
 JOIN payment ON staff.staff_id = payment.staff_id
-WHERE DATE(payment.payment_date) >= '2020-01-01'
-  AND DATE(payment.payment_date) < '2020-02-01'
-GROUP BY staff.staff_id, staff.first_name, staff.last_name;
+WHERE to_char(payment.payment_date, 'YYYY-MM-DD') >= '2020-01-01'
+  AND to_char(payment.payment_date, 'YYYY-MM-DD') < '2020-02-01'
+GROUP BY staff.staff_id, staff.first_name, staff.last_name
+ORDER BY sum;
 
